f4200676e1e0ff116ed2f7512cbf443109c900bd
jemojeremmy2@gmail.com <jemojeremmy2>
2024-05-09 05:29:21 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
     <Router>
       <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/services" element = {<Services/>}/>
+      <Route path="/services" element = {<Services/>}/> 
       <Route path="/about" element = {<AboutUs/>}/>
       </Routes>
     </Router>
